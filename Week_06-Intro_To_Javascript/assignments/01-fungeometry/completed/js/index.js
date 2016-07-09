26920e505cbde51eadda98bea8ba244ff837f008
jyeedesign <jyeedesign@gmail.com>
2016-07-09 10:47:39 -0700

    test

---
+++ Week_06-Intro_To_Javascript/assignments/01-fungeometry/completed/js/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var today = new Date();
+var hourNow = today.getHours();
+var greetings;
+
+if (hourNow > 18) {
+	greeting = 'Good afternoon!';
+} else if (hourNow > 12) {
+	greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Welcome';
+}
+
+document.write('<h3>' + greeting + '</h3>');
