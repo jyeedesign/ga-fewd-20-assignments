fdc94ca07515f0273aa63cc4f8022907550e7533
jyeedesign <jyeedesign@gmail.com>
2016-07-16 13:54:51 -0400

    fungeometry

--- Week_06-Intro_To_Javascript/assignments/01-fungeometry/completed/js/index.js
+++ Week_06-Intro_To_Javascript/assignments/01-fungeometry/completed/js/index.js
@@ -1,15 +1,71 @@
 "use strict";
 
-var today = new Date();
-var hourNow = today.getHours();
-var greetings;
+function main () {
 
-if (hourNow > 18) {
-	greeting = 'Good afternoon!';
-} else if (hourNow > 12) {
-	greeting = 'Good afternoon!';
-} else if (hourNow > 0) {
-    greeting = 'Welcome';
+	var diamond1 = document.getElementById("diamond1");
+	var diammond2 = document.getElementById("diamond2");
+	var diamond3 = document.getElementById("diamond3");
+	var diamond4 = document.getElementById("diamond4");
+
+	diamond1.addEventListener("click", clickedDiamond1);
+	diamond2.addEventListener("click", clickedDiamond2);
+	diamond3.addEventListener("click", clickedDiamond3);
+	diamond4.addEventListener("click", clickedDiamond4);
 }
 
-document.write('<h3>' + greeting + '</h3>');
+function clickedDiamond1 () {
+	var diamond1 = document.getElementById("diamond1");
+	var diammond2 = document.getElementById("diamond2");
+	var diamond3 = document.getElementById("diamond3");
+	var diamond4 = document.getElementById("diamond4");
+
+	diamond1.style.transform = "rotate (45deg)";
+	diamond2.style.transform = "rotate (67deg)";
+	diamond3.style.transform = "rotate (22deg)";
+	diamond4.style.transform = "rotate (45deg)";
+
+}
+
+function clickedDiamond2 () {
+	var diamond1 = document.getElementById("diamond1");
+	var diammond2 = document.getElementById("diamond2");
+	var diamond3 = document.getElementById("diamond3");
+	var diamond4 = document.getElementById("diamond4");
+
+	diamond1.style.transform = "rotate (22deg)";
+	diamond2.style.transform = "rotate (45deg)";
+	diamond3.style.transform = "rotate (45deg)";
+	diamond4.style.transform = "rotate (67deg)";
+
+}
+
+function clickedDiamond3 () {
+	var diamond1 = document.getElementById("diamond1");
+	var diammond2 = document.getElementById("diamond2");
+	var diamond3 = document.getElementById("diamond3");
+	var diamond4 = document.getElementById("diamond4");
+
+	diamond1.style.transform = "rotate (67deg)";
+	diamond2.style.transform = "rotate (45deg)";
+	diamond3.style.transform = "rotate (45deg)";
+	diamond4.style.transform = "rotate (22deg)";
+
+}
+
+function clickedDiamond4 () {
+	var diamond1 = document.getElementById("diamond1");
+	var diammond2 = document.getElementById("diamond2");
+	var diamond3 = document.getElementById("diamond3");
+	var diamond4 = document.getElementById("diamond4");
+
+	diamond1.style.transform = "rotate (45deg)";
+	diamond2.style.transform = "rotate (22deg)";
+	diamond3.style.transform = "rotate (67deg)";
+	diamond4.style.transform = "rotate (45deg)";
+
+}
+
+window.addEventListener("load", main);
+
+
+
