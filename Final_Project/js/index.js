d8045e1b66e5ca47e3001f6c502bc2a882f98448
jyeedesign <jyeedesign@gmail.com>
2016-07-23 17:16:31 -0700

    Final project

--- Final_Project/js/index.js
+++ Final_Project/js/index.js
@@ -1,50 +1,18 @@
-(function() {
 
-var win = $(window);
+"use strict";
 
-win.resize(function() {
-    
-    var win_w = win.width(),
-        win_h = win.height(),
-        $bg    = $("#bg");
+var startup = function () {
+  var body = $("body");
+  var quotes = $("main#quotes");
 
-    // Load narrowest background image based on 
-    // viewport width, but never load anything narrower 
-    // that what's already loaded if anything.
-    var available = [
-      1024, 1280, 1366,
-      1400, 1680, 1920,
-      2560, 3840, 4860
-    ];
+  quotes.on("mouseover", function () {
+    body.addClass("isenabled");
+  });
 
-    var current = $bg.attr('src').match(/([0-9]+)/) ? RegExp.$1 : null;
-    
-    if (!current || ((current < win_w) && (current < available[available.length - 1]))) {
-      
-      var chosen = available[available.length - 1];
-      
-      for (var i=0; i<available.length; i++) {
-        if (available[i] >= win_w) {
-          chosen = available[i];
-          break;
-        }
-      }
-      
-      // Set the new image
-      $bg.attr('src', '/img/bg/' + chosen + '.jpg');
-      
-      // for testing...
-      // console.log('Chosen background: ' + chosen);
-      
-    }
+  quotes.on("mouseout", function () {
+    body.removeClass("isenabled");
+  });
 
-    // Determine whether width or height should be 100%
-    if ((win_w / win_h) < ($bg.width() / $bg.height())) {
-      $bg.css({height: '100%', width: 'auto'});
-    } else {
-      $bg.css({width: '100%', height: 'auto'});
-    }
-    
-  }).resize();
-  
-})(jQuery);
+};
+
+$(document).ready(startup);
